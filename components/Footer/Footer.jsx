df5b84b40a0b78c9be73ef8f02f693f3efcdc4a8
sohamsshah <sohamshah456@gmail.com>
2021-10-04 15:27:04 +0530

    :rocket: completed footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,7 +1,64 @@
 import React from "react";
-
+import Link from "next/link";
+import { MdiTwitter, MdiEmail, MdiGithub, MdiLinkedin } from "../svgs/Socials";
 const Footer = () => {
-  return <div>I am Footer</div>;
+  return (
+    <footer className="p-10 flex justify-center items-center">
+      <div className="flex flex-col items-center">
+        <ul className="flex text-3xl">
+          <li className="m-2">
+            <a
+              className="hover:text-blue-900"
+              href="https://twitter.com/sohamsshah_"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <MdiTwitter />
+            </a>
+          </li>
+          <li className="m-2">
+            <a
+              className="hover:text-blue-900"
+              href="https://github.com/sohamsshah"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <MdiGithub />
+            </a>
+          </li>
+          <li className="m-2">
+            <a
+              className="hover:text-blue-900"
+              href="https://www.linkedin.com/in/sohamshah456/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <MdiLinkedin />
+            </a>
+          </li>
+          <li className="m-2">
+            <a
+              className="hover:text-blue-900"
+              href="mailto:sohamshah456@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <MdiEmail />
+            </a>
+          </li>
+        </ul>
+        <div className="text-gray-600 flex flex-wrap">
+          <div>Copyright © 2021 </div>
+          <div>
+            •{" "}
+            <Link href="/">
+              <a>Soham 👨‍💻</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
